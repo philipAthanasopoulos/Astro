72dee6c71fe47850e8f7f376792c339b20f3173c
philipAthanasopoulos <filipposfivos@gmail.com>
2024-04-16 16:35:55 +0300

    updated README.md

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -5,6 +5,10 @@ module org.example.demo {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.apache.lucene.queries;
+    requires org.apache.lucene.queryparser;
+    requires org.apache.lucene.core;
+
 
     opens org.example.demo to javafx.fxml;
     exports org.example.demo;
