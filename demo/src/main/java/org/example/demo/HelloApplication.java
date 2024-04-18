9bce4f11948b0addd0d8f6bb6afb24f810b9e8ec
philipAthanasopoulos <filipposfivos@gmail.com>
2024-04-18 20:26:06 +0300

    added small demo

--- demo/src/main/java/org/example/demo/HelloApplication.java
+++ demo/src/main/java/org/example/demo/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Searchio!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
